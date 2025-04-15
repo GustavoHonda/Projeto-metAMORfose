d06abb05815c9e457cb0d1bbd9fee9ce51e9eef3
gustavo_honda <gustavo_honda@usp.br>
2025-04-15 20:23:08 -0300

    change matching algorithm

--- get_data.py
+++ get_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import json
 import requests
 
+
 def data_info(df,column):
     print(df[column].unique())
     print(df[column].dtype)
@@ -12,10 +13,8 @@ with open("../key/sheets_url.json") as file:
 
 
 def preprocess_respostas(df):
-        
         name_columns= ['time','name_paciente','e-mail','phone_paciente','area','description','max_price','price','phone2','urgencie','free_service','sexual_identity','profissional','whatsapp']
         df.columns = name_columns
-        
         type_columns=[str,str,str,int,str,str,int,int,int,str,str,str,str,str]
         
         # df = df.reset_index()
@@ -34,15 +33,14 @@ def preprocess_respostas(df):
         df[['date','time']] = df['time'].str.split(" ", expand= True )
         return df
 
+
 def preprocess_profissional(df) -> pd.DataFrame:
-        
         # Adding new data
         df['freq'] = '0'
         
         # Rename columns
         name_columns = ['name_professional','area','CRN','phone_professional','price','gender','freq']
         df.columns = name_columns
-        
         
         # Remove unwanted data
         df = df.dropna(axis=1)
@@ -52,9 +50,9 @@ def preprocess_profissional(df) -> pd.DataFrame:
         # Setting correct types
         type_columns = [str,str,str,int,int,object,str,int]
         for i,coluna in enumerate(df.columns):
-                df.loc[:,coluna] = df[coluna].astype(str).astype(type_columns[i])
-        
+                df.loc[:,coluna] = df[coluna].astype(str).astype(type_columns[i])        
         return df
+
 
 def open_respostas():
         sheets_respostas = data['url_respostas']
@@ -62,11 +60,13 @@ def open_respostas():
         df = preprocess_respostas(df)
         return df
 
+
 def open_profissional():
         sheets_professional = data['url_profissionais']
         df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheets_professional}/export?format=csv")
         df = preprocess_profissional(df)
         return df
+
 
 def send_data(row, col, value):
         payload = {"row": row, "col": col, "value": value}
@@ -76,24 +76,20 @@ def send_data(row, col, value):
                 print("Request Accepted!")
         else:
                 print(response)
-        
         return response
 
 
-
 def main():
+        # Get dataframes of resposta & profissional
         df_resposta = open_respostas()
         df_profissional = open_profissional()
         
-        response = send_data(40,1,"Modificado")
+        print(df_resposta)
         
-        # df_resposta = pd.merge(df_profissional, df_resposta, on=['area','price'], how='inner')
-        # result_professional = df_profissional.loc[df_profissional['area'] == "Psiquiatria"]
-        # result_resposta = df_resposta.loc[df_resposta['area'] == "Psiquiatria"]
-        
+        # Modify value from sheets table obs: (ainda tem erro ao modificar tabela, depois não é possível obter as informações da planilha)
+        # response = send_data(40,1,"Modificado")
+
 
 if __name__ == "__main__":
         main()
-
-
-
+        
--- send_msg.py
+++ send_msg.py
@@ -10,13 +10,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
+# (É necessário rodar esse comando no terminal para enviar as mensagens pelo whatsapp web)
 # xhost + local:
 
 def exit_webpg():
-    time.sleep(2)
-    pg.keyDown('ctrl')
-    pg.press('w')
-    pg.keyUp('ctrl')
+    try:
+        time.sleep(2)
+        pg.keyDown('ctrl')
+        pg.press('w')
+        pg.keyUp('ctrl')
+    except RuntimeError:
+        print("Exit error")
+        return -1
 
 def direct_msg(phone, message):
     web.open("https://web.whatsapp.com/send?phone="+phone+"&text="+message)
@@ -31,19 +36,14 @@ def open_page():
         raise ConnectionError
     else:
         print("Connection established")    
-    
     return response
 
-def check_load():
+def check_load(): # Ainda não funcional
     chrome_options = Options()
     chrome_options.binary_location = "/usr/local/bin/google-chrome"
-
     service = Service(executable_path="/usr/bin/chromedriver") 
-
     driver = webdriver.Chrome(service=service, options=chrome_options)
     driver.get("https://web.whatsapp.com")
-
-    # Espera até que a barra de busca esteja presente (indicando que a interface carregou)
     try:
         print("Aguardando carregamento do WhatsApp Web...")
         search_box = WebDriverWait(driver, 120).until(
@@ -55,26 +55,26 @@ def check_load():
     driver.quit()
     
 def send_msg(phone, message):
-    search_bar = pg.locateOnScreen('search_bar.png') #Search in printscreen for search_bar
-    search_bar_x, search_bar_y = pg.center(search_bar) #Get coordinates
+    search_bar = pg.locateOnScreen('search_bar.png') 
+    search_bar_x, search_bar_y = pg.center(search_bar) 
     pg.click(search_bar_x, search_bar_y)
-    pg.write(phone) #Search phone number
-    pg.press('enter') #Select first match
+    pg.write(phone) 
+    pg.press('enter') 
     time.sleep(1)
-    pg.write(message) #Write message
-    pg.press("enter") #Send
-    pg.click(search_bar_x + 250, search_bar_y) #Erase phone number from searchbar
+    pg.write(message) 
+    pg.press("enter") 
+    pg.click(search_bar_x + 250, search_bar_y) 
 
 
 if __name__ == '__main__':
     df_respostas = open_respostas()
     df_profissional = open_profissional()
     
-    phone = "11950440023"
-    message = "Hello pyautogui!"
+    phone = "11976154853"
+    message = "bhjkbbkjlkl"
     response = open_page()
-    status = check_load()
-    # time.sleep(10)
-    # send_msg(phone, message)
-    # time.sleep(3)
+    # status = check_load()
+    time.sleep(7)
+    send_msg(phone, message)
+    time.sleep(3)
     # exit_webpg()
