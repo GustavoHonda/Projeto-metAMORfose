0780389945506ab166a8774036b313c1da8c2929
gustavo_honda <gustavo_honda@usp.br>
2025-04-23 20:24:46 -0300

    Add datetime type assert test

--- test/get_data_test.py
+++ test/get_data_test.py
@@ -1,6 +1,5 @@
 import pandas as pd
-from ..get_data import extrair_precos, preprocess_respostas
-
+from src.get_data import extrair_precos, preprocess_respostas
 
 
 def test_extrair_precos_simples():
@@ -22,7 +21,7 @@ def test_extrair_precos_com_ponto_milhar():
 def test_preprocess_respostas_minimal(monkeypatch):
     # Simula um dataframe de entrada
     df = pd.DataFrame([{
-        'time': '12/04/2024 14:00',
+        'time': '22/12/2024 21:10:00',
         'name_paciente': 'João',
         'e-mail': 'joao@email.com',
         'phone_paciente': '11999999999',
@@ -39,6 +38,8 @@ def test_preprocess_respostas_minimal(monkeypatch):
     }])
 
     df = preprocess_respostas(df)
+    assert df['datetime'].dtype == "datetime64[ns]"
+    assert df['datetime'].iloc[0] == pd.to_datetime('2024-12-22 21:10:00')
     assert df['price'].iloc[0] == [200.0]
     assert df['area'].iloc[0] == 'Psicologia'
     assert 'datetime' in df.columns
