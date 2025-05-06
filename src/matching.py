de651ef0d4e8519a18f9cc66240c04852b906dd0
gustavo_honda <gustavo_honda@usp.br>
2025-05-06 17:40:22 -0300

    Clean matching

--- src/matching.py
+++ src/matching.py
@@ -1,8 +1,5 @@
 import pandas as pd
 from pandasql import sqldf
-import gspread
-from gspread_dataframe import set_with_dataframe
-from oauth2client.service_account import ServiceAccountCredentials
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -15,66 +12,9 @@ from src.get_data import data_info, open_matches, save_matches, open_profissiona
 # 4. cada profissional deve receber clientes diferentes
 
 
-def find_matches(prof, df_resposta, df_matches):
-    # query = "SELECT * FROM df_matches"
-    # df = ps.sqldf(query,locals())
-   # profissional  = prof[["name_professional", "phone_professional", "area", "price"]]
-  #  paciente  = df_resposta[["datetime", "name_paciente", "phone_paciente", "area", "price"]]
-
-  pass
-
-   # already_matched_professional_pacient = df_matches[df_matches["phone_professional"] == prof["phone_professional"]]
-   # new_matches = df_resposta[df_resposta["area"] == prof["area"]]
-    
-   # new_matches = new_matches[new_matches["datetime"].isin(already_matched_professional_pacient["datetime"])]
-    # new_matches = new_matches.sort_values(by="datetime")
-    
-    # new_matches['datetime'] =new_matches["datetime"].apply(lambda x: x.isoformat() if pd.notnull(x) else None)
-    
-    # return new_matches.head(2)
-
-
-def match(df_profissional, df_resposta):
-    df_matches = open_matches()
-    results = []
-    for _, prof in df_profissional.iterrows():
-        best_matches = find_matches(prof, df_resposta, df_matches)
-        for _, match in best_matches.iterrows():
-            results.append({
-                "phone_professional": prof["phone_professional"],
-                "name_paciente": match["name_paciente"],
-                "phone_paciente": match["phone_paciente"],
-                "area": match["area"],
-                "price": match["price"][0],
-                "datetime": match["datetime"]
-            })
-    new_matches = pd.DataFrame(results)
-    if not new_matches.empty:
-        combined = pd.concat([df_matches, new_matches], ignore_index=True)
-        save_matches(combined)
-        print("Novos pareamentos salvos em df_matches.csv:")
-        print(new_matches)
-    else:
-        print("Nenhum novo pareamento encontrado.")
-        
-    return new_matches
-
-
 def main():
     df_profissional = open_profissional()[["name_professional","area", "phone_professional", "price", "freq_professional"]]
     df_resposta = open_respostas()[["name_paciente", "area", "datetime", "phone_paciente", "price","freq_client"]]
-   # df_resposta = df_resposta.head(3)
-    # print(df_resposta)
-   # data_info(df_profissional, "area")
-   # data_info(df_resposta, "area")
-
-   # matched = match(df_profissional, df_resposta)
-    
-   # print(matched)
-    
-   # pysqldf = lambda q: sqldf(q, globals())
-    
-    #pysqldf = lambda q: sqldf(q, locals())
 
     query = """
     SELECT *
@@ -110,20 +50,10 @@ def main():
 
     # Ginecologia = caminho_arquivo_csv.where(caminho_arquivo_csv['area_comum'] = 'Ginecologia')
 
-    # Salvar em Excel
-    resultado.to_excel(caminho_arquivo_excel, index=False)
-
     # Salvar em CSV
     resultado.to_csv(caminho_arquivo_csv, index=False)
 
-
-
-
-    print(f"Arquivos salvos em: {caminho_arquivo_excel} e {caminho_arquivo_csv}")
-
-
-# 1. Ler o Excel com os matches
-    df_resultado = pd.read_excel(caminho_arquivo_excel)
-
+    print(f"Arquivos salvos em: {caminho_arquivo_csv}")
+    df_resultado = pd.read_csv(caminho_arquivo_csv)
 if __name__ == "__main__":
     main()
